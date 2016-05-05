aca9758a2a5af0d6512e74c8ed5fbccb94b33d42
Donovan An <donovantaean@yahoo.com>
2016-05-04 17:31:02 -0700

    added rsvp summary page

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,4 +2,8 @@ class MainController < ApplicationController
   	def index
 
   	end
+  	def showallrsvps
+  		@rsvps = Rsvp.all
+  		puts @rsvps.inspect
+  	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   post '/rsvps' => "rsvps#create"
 
-  
+  get '/showallrsvps' => "main#showallrsvps"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
